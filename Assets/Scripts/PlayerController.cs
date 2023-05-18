3da82a07406957ad0ce6fd2cf3056267acd4c0ae
NaniSore7 <lavrovskiy.v@bk.ru>
2023-05-18 12:31:15 +0300

    Movement and input system
    Added player controls, new input system and joystick.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,62 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Timeline;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private PlayerControls playerInput;
+    private Vector2 moveDirection = Vector2.zero;
+
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float rotationSpeed = 10f;
+    private float rotationSpeedMultiplier = 50f;
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        playerInput = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnEnable()
+    {
+        playerInput.Enable();
+        playerInput.Player.Movement.performed += OnMovementPerformed;
+        playerInput.Player.Movement.canceled += OnMovementCanceled;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.velocity = moveDirection * moveSpeed;
+
+
+        if (moveDirection != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, moveDirection);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * rotationSpeedMultiplier * Time.deltaTime);
+            rb.MoveRotation(rotation);
+        }
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+        playerInput.Player.Movement.performed -= OnMovementPerformed;
+        playerInput.Player.Movement.canceled -= OnMovementCanceled;
+    }
+    
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        moveDirection = value.ReadValue<Vector2>();
+    }
+
+    private void OnMovementCanceled(InputAction.CallbackContext value)
+    {
+        moveDirection = Vector2.zero;
     }
 }
